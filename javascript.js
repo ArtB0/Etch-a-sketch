5fa054a6adf958138354654d9b51fc56d03173bb
Art <artbogdanovics@gmail.com>
2022-09-27 21:43:49 +0300

    added button

--- javascript.js
+++ javascript.js
@@ -1,33 +1,49 @@
-const container = document.querySelector(".container");
-let rows = document.querySelector(".gridRow");
-let columns = document.querySelector(".gridColumns");
 
-//let number = parseInt(prompt('Grid size' , '16'), 100);
 
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+
+
+function makeGrid() {
+let number = parseInt(prompt('Grid size' , '16'));
+  makeRows(number);
+  makeColumns(number);
+}
+
+
+function makeRows(rowNum) {
+  for (r = 0; r < rowNum; r++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "gridRow";
+  };
+};
+
+function makeColumns(cellNum) {
+  for (i = 0; i < rows.length; i++) {
+    for (j = 0; j < cellNum; j++) {
+      let newCell = document.createElement("div");
+      rows[j].appendChild(newCell).className = "cell";
+    };
+
+  };
+};
+
+
+
+// Button
+let gridButton = document.querySelector('#gridButton');
+gridButton.addEventListener('click' , function() {
 makeGrid();
-
-function makeGrid () {
-    let number = 16;
-    makeRows(number);
-    makeColumns(number);
-}
+});
 
 
-function makeRows(rowNumber) {
-    for (r=0; r < rowNumber; r++) {
-        let row = document.createElement('div');
-        container.appendChild(row).className = 'gridRow';
-    }
-}
 
-function makeColumns(cellNumber) {
-    for (i=0; i < rows.length; i++) {
-        for (j=0; j < cellNumber; j++){
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = 'cell';
-        }
-    }
-}
+
+
+
+
 
 
 
